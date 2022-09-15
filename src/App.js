df65e9206e70416fe24a1af90b2a4f9a70032328
wulzymart <olawulzy@yahoo.co.uk>
2022-09-15 11:16:14 +0100

    header built

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage";
 import { Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop_page";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="">
+      <Header />
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="/shop" element={<ShopPage />} />
