dbc3126a39077eb7c3336cc2a11ac861ac2f2f5d
wulzymart <olawulzy@yahoo.co.uk>
2022-09-28 14:34:09 +0100

    optimized for production by editing middleware setting#

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,9 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 
-const middleWare = [logger];
+const middleWare = [];
+process.env.NODE_ENV === "development" && middleWare.push(logger);
+
 const store = createStore(rootReducer, applyMiddleware(...middleWare));
 export const persistor = persistStore(store);
 
